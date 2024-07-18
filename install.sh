31b4b4e70bcaf4e22b7141bfb1e76e351ceaf151
sungongwei <273965695@qq.com>
2024-07-18 19:19:19 +0800

    fix:

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update -y
 apt install -y  sox libsndfile1 bzip2   build-essential
-pip install  --use-pep517 pytest-runner paddlepaddle paddlespeech numpy==1.23.5 onnxruntime==v1.14.0 #-i https://pypi.tuna.tsinghua.edu.cn/simple 
+pip install  --use-pep517 pytest-runner paddlepaddle==2.5.1 paddlespeech numpy==1.23.5 onnxruntime==v1.14.0 #-i https://pypi.tuna.tsinghua.edu.cn/simple 
 
 
 wget -nc https://paddlespeech.bj.bcebos.com/Parakeet/tools/nltk_data.tar.gz
--- readme.md
+++ readme.md
@@ -3,8 +3,13 @@
 
 - ubuntu 20.04
 - python3.8
+- paddlepaddle2.5.1
 - cpu
-  - ./install.sh
-  - ./start.sh or ./start_muti.sh
-  - ./test.sh
+  - none
 - gpu
+  - cuda11.8
+  - cudnn8.1.1
+- start
+  - ./install.sh
+  - ./start.sh
+  - ./test.sh
